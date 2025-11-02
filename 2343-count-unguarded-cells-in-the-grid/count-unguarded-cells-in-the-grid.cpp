3fc7a2d38806f086cc2a1c5a19ccf63f50572133
hadygust <94296915+hadygust@users.noreply.github.com>
2025-11-02 20:56:23 +0700

    Time: 513 ms (11.01%) | Memory: 212.8 MB (16.06%) - LeetSync

---
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        vector<vector<int>> board(m, vector<int>(n));
+        map<pair<int, int>, int> mem;
+
+        for(int i = 0; i < guards.size(); i++){
+            board[guards[i][0]][guards[i][1]] = 1;
+            mem[{guards[i][0], guards[i][1]}]++;
+        }
+
+        for(int i = 0; i < walls.size(); i++){
+            board[walls[i][0]][walls[i][1]] = 2;
+        }
+
+        for(auto g : guards){
+            int i = g[0];
+            int j = g[1];
+            while(i >= 0){
+                if(board[i][j] == 2) break;
+                if(mem.find({i,j}) != mem.end() && (i != g[0] || j != g[1])) break;
+                board[i][j] = 1;
+                i--;
+            }
+
+            i = g[0];
+            j = g[1];
+            while(i < m){
+                if(board[i][j] == 2) break;
+                if(mem.find({i,j}) != mem.end() && (i != g[0] || j != g[1])) break;
+                board[i][j] = 1;
+                i++;
+            }
+
+            i = g[0];
+            j = g[1];
+            while(j >= 0){
+                if(board[i][j] == 2) break;
+                if(mem.find({i,j}) != mem.end() && (i != g[0] || j != g[1])) break;
+                board[i][j] = 1;
+                j--;
+            }
+
+            i = g[0];
+            j = g[1];
+            while(j < n){
+                if(board[i][j] == 2) break;
+                if(mem.find({i,j}) != mem.end() && (i != g[0] || j != g[1])) break;
+                board[i][j] = 1;
+                j++;
+            }
+
+            // for(int i =0; i < board.size(); i++){
+            //     for(int j = 0; j < board[i].size(); j++){
+            //         cout << board[i][j] << " ";
+            //     }
+            //     cout << endl;
+            // }
+            // cout << endl;
+        }
+
+        int res = 0;
+        for(int i =0; i < board.size(); i++){
+            for(int j = 0; j < board[i].size(); j++){
+                if(board[i][j] == 0) res++;
+            }
+        }
+
+        return res;
+    }
+
+};
