7c12e09981d408c9ea3e7203235e5214624be71a
hadygust <94296915+hadygust@users.noreply.github.com>
2025-11-04 14:25:02 +0700

    Time: 177 ms (5.11%) | Memory: 37.5 MB (15.33%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.cpp
@@ -2,34 +2,26 @@ class Solution {
 public:
     vector<int> findXSum(vector<int>& nums, int k, int x) {
         vector<int> res;
-        int count = 0;
-
         vector<int> curr;
-
-
+        
         for(int i = 0; i < k-1; i++){
             curr.push_back(nums[i]);
         }
 
         for(int i = 0; i <= nums.size()-k; i++){
-            // cout << "iter" << endl;
             int back = k-1+i;
             curr.push_back(nums[back]);
 
             map<int, int> mp;
             priority_queue<pair<int, int>> pq;
 
-            // cout << "CURR: " << endl;
             for(int n : curr){
                 cout << n << " ";
                 mp[n]++;
             }
-            // cout << endl;
 
-            // cout << "PQ PUSH: " << endl;
             for(auto p : mp){
                 pq.push({p.second, p.first});
-                // cout << p.second << " " << p.first << endl;
             }
 
             int count = 0;
@@ -41,20 +33,15 @@ public:
                 }
                 res.push_back(count);
                 curr.erase(curr.begin());
-                // cout << endl;
                 continue;
             }
 
-            // cout << "PQ: " << endl;
             for(int j = 0; j < x; j++){
-                // cout << pq.top().second << " " << pq.top().first << endl;
                 count += pq.top().second*pq.top().first;
                 pq.pop();
             }
-            // cout << endl;
 
             res.push_back(count);
-
             curr.erase(curr.begin());
 
         }
