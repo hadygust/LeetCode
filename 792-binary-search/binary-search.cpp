26e255086f9339e0635d953d9542c2f9de94913d
hadygust <94296915+hadygust@users.noreply.github.com>
2026-03-14 22:38:53 +0700

    Time: 4 ms (1.12%) | Memory: 31.4 MB (45.21%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size()-1, curr = (r-l)/2;
+        if(nums[0] == target) return 0;
+        while(l <= r){
+            cout << curr << endl;
+            if(nums[curr] == target) return curr;
+            else if(nums[curr] < target){
+                l = curr+1;
+                curr = (r-l)/2+l;
+            } else {
+                r = curr-1;
+                curr = (r-l)/2+l;
+            }
+        }
+
+        return -1;
+    }
+};
