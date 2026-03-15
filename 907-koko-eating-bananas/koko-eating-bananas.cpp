2fb65174b55ea098810fb3e75125da5a2cfee4cc
hadygust <94296915+hadygust@users.noreply.github.com>
2026-03-16 02:27:56 +0700

    Time: 17 ms (11.27%) | Memory: 22.8 MB (77.74%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        // cout << ceil((double)3/2) << endl;
         int l = 1, r = 0;
 
         for(int n : piles){
             if(n > r) r = n;
         }
-
-        // cout << "r " << r << endl;
 
         int curr = ceil((double)(r-l)/2);
         int ans = r;
@@ -31,20 +28,6 @@ public:
                 curr = (r - l) / 2 + l;
             }
         }
-
-        // cout << curr << endl;
-
-        // while (curr > 1){
-        //     curr--;
-        //     int time = 0;
-        //     for(int n : piles){
-        //         time += ceil((double)n/curr);
-        //     }
-        //     if(time != h || curr == 0){
-        //         curr++;
-        //         break;
-        //     } 
-        // }
 
         return curr;
     }
